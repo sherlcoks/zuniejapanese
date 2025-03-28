ca83dfe6a609fb0fc35afb6197c6c38b4c240d75
MSI\atash <damngochau123@gmail.com>
2025-03-29 00:02:15 +0900

    Build assets for production

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,10 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    base: '/', // nếu dự án được phục vụ từ root
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
